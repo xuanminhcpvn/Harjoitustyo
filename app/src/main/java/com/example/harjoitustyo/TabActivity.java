a17a293ce1d35964f784c76a073c4c68f3652b03
Minh Pham <xuanminhcpvn@gmail.com>
2024-04-18 16:39:15 +0300

    TabActivity ja TabPagerAdapter kunnossa voi testaa  syöttää yksittäisiä datoja textViewnä jos haluaa

--- app/src/main/java/com/example/harjoitustyo/TabActivity.java
+++ app/src/main/java/com/example/harjoitustyo/TabActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class TabActivity extends AppCompatActivity {
 
@@ -20,5 +23,35 @@ public class TabActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager2 viewArea = findViewById(R.id.viewArea);
+        TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(this);
+        viewArea.setAdapter(tabPagerAdapter);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewArea.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewArea.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
     }
-}
+
+    }
--- app/src/main/java/com/example/harjoitustyo/TabPagerAdapter.java
+++ app/src/main/java/com/example/harjoitustyo/TabPagerAdapter.java
@@ -1,4 +1,35 @@
 package com.example.harjoitustyo;
 
-public class TabPagerAdapter{
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class TabPagerAdapter extends FragmentStateAdapter {
+
+    public TabPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new FragmentInfo();
+            case 1:
+                return new FragmentCompare();
+            case 2:
+                return new FragmentQuiz();
+            default:
+                return new FragmentInfo();
+
+
+        }
+
+    }
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
 }
+
